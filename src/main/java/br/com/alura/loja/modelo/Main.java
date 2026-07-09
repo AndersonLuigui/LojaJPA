5f8dbcd146326538a4431682d643a05d0584c234
Silva <anderson.lsilva@br.experian.com>
2026-07-08 23:17:39 -0300

    update II

--- src/main/java/br/com/alura/loja/modelo/Main.java
+++ src/main/java/br/com/alura/loja/modelo/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package br.com.alura.loja.modelo;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
